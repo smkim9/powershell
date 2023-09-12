3260bc9630542d5a4150702c33e261a07b426f6e
smkim9 <seongmin.kim@outlook.com>
2023-09-12 12:47:35 -1000

    some changes

--- function New-BulkADUser{.ps1
+++ function New-BulkADUser{.ps1
@@ -4,7 +4,7 @@ function New-BulkADUser{
     )
     Import-Module ActiveDirectory 
 
-    $userlist = Import-Csv $CSVFilePath <Filepath>
+    $userlist = Import-Csv $CSVFilePath #<Filepath>
 
     foreach($user in $userlist){
         $FirstName = $user.FirstName
@@ -25,3 +25,65 @@ function New-BulkADUser{
         write-host "An error occurred: $($Error.Exception.Message)"
     }
 }
+
+function New-UserInput {
+    param(
+        [string]$FirstName, 
+        [string]$LastName, 
+        [string]$Username, 
+        [SecureString]$Password, 
+        [string]$Department, 
+        [string]$OU 
+    )
+    if (-not $FirstName) {
+        $FirstName = Read-Host "Enter the first name"
+    }
+    if (-not $LastName) {
+        $LastName = Read-Host "Enter the last name"
+    }
+    if (-not $Username) {
+        $Username = Read-Host "Enter the username"
+    }
+    if (-not $Password) {
+        $Password = Read-Host "Enter the password" -AsSecureString
+    }
+    if (-not $Department) {
+        $Department = Read-Host "Enter the department"
+    }
+    if (-not $OU) {
+        $OU = Read-Host "Enter the OU (Organizational Unit)"
+    }
+    $commonName = $firstname + ' ' + $LastName
+    New-ADUser -Name $commonName -UserPrincipalName $firtname + "@contoso.com" -GivenName $firstname -Surname $LastName `
+    -Department $Department -Path $OU -AccountPassword (Converto-SecureString $password -AsPlainText -Force) -Enabled $true
+}
+
+function Set-ADEnvironment{
+    Param(
+        [string]$OU,
+        [string]$Group,
+        [string]$UserToAdd
+    )
+    #create OU
+    $ouPath = "OU=$OU,DC=Adatum,DC=com"
+    if(-not(Get-ADOrganizationalUnit -Filter {Name -eq $OU})){
+        New-ADOrganizaitonalUnit -Name $OU -Path "DC=Adatum,DC=com"
+        Write-Host"$ou created"
+    }
+    #creates group
+    $groupPath = "CN=$group,OU=$OU,DC=Adatum,DC=Com"
+    if(-not (Get-ADGroup -Filter{Name -eq $Group})){
+        New-ADGroup -Name $Group -GroupScope Global -GroupCategory Security -Path $ouPath
+        Write-Host "$Group created"
+    }    
+    $user = Get-ADUser -Filter{SamAccountName -eq $UserToAdd}
+    if($user = $UserToAdd){
+        Add-GroupMember -Identity $groupPath -Member $user
+        Write-Host "Added $($user.SamAccountName) to Group: $GroupName"
+    } else{
+        Write-Host "User not found"
+    }
+}
+#$OUName = Read-Host "Enter OU Name"
+#$GroupName = Read-Host "Enter Group Name"
+#$UserToAdd = Read-Host "Enter User to Add"
--- locate and unlock a specific user accou.ps1
+++ locate and unlock a specific user accou.ps1
@@ -1,15 +1,65 @@
 #locate and unlock a specific user account
-
+Import-Module ActiveDirectory
 $lockedAccount = Search-ADAccount -Lockedout
 
 $UserToUnlock = Read-Host "Enter Username to unlock"
 
-foreach($user in $UsertoUnlock){
-    if($lockedaccount -eq $usertounlock){
-        Unlock-ADAccount -identity $Usertounlock
+foreach($user in $UserToUnlock){
+    if($lockedaccount -eq $UserTounlock){
+        Unlock-ADAccount -identity $UserTounlock
         Write-Host "User account has been unlocked"
     }
     else{
     Write-Host "User account not found"
+    }
+}
+#expand on the script above
+
+$passwordLog = "C:\PasswordResetLog.txt"
+$Administrator = [bool](New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())) | Out-Null
+
+if($Administrator -eq $false){
+    Write-Host "You do not hold power here. Contact your administrator"
+    $logEntry= "$(get-date) unaurhotrized access attempted by $env:USERNAME"
+    Add-Content -Path $PasswordLog -value $logentry
+
+    Exit
+}
+$username = Read-Host "Enter your Username"
+
+$account = Get-ADUser -Filter{SamAccountName -eq $username}
+if($account -eq $null){
+    write-host "Account not found"
+    $logEntry= "$(get-date) $account not found"
+    Add-Content -Path $PasswordLog -value $logentry
+
+} elseif ($account.Enabled -eq $false){
+    Write-Host "Account locked. Would you like reset the password?"
+    Write-Host "Enter Y to unlock"
+    $answer = Read-Host
+    if($answer -eq "Y" -or $answer -eq "y"){
+        Unlock-ADAccount -Identity $account
+        $newPassword = Read-Host "Enter your new password" -AsSecureString
+        Set-ADAccountpassword -Identity $account -NewPassword $newPassword -Reset
+        Write-Host "Passwword reset successful"
+
+        $logEntry= "$(get-date) Password reset successful for user $account"
+        Add-Content -Path $PasswordLog -value $logentry
+    }
+    else{
+        write-host "Tough luck"
+        $logEntry= "$(get-date) Account remains locked for $account"
+        Add-Content -Path $PasswordLog -value $logentry
+    }
+    else {
+        $newPassword = Read-Host "Enter the new password" -AsSecureString
+
+        Set-ADAccountPassword -Identity $account -NewPassword $newPassword -Reset
+        Write-Host "Password reset success"
+
+        $logEntry= "$(get-date) password reset success"
+        Add-Content -Path $PasswordLog -value $logentry
+
+
     }
 }
